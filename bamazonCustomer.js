f6a3e7bded49639ba1a4ce4e9c4f21694c42d399
Anthony Cera <ace_cera@yahoo.com>
2018-02-20 18:47:24 -0800

    took out some random syntax that was preventing the table from displaying.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Table = require('cli-table');
 
 const DATABASE = 'bamazon_db';
 const TABLE = 'products';
@@ -15,8 +16,52 @@ const connection = mysql.createConnection({
 
 connection.connect(function(error) {
     if (error) {
-        console.log(error);
+        throw error;
     }
-
-    console.log(`Connect as id ${connection.threadId}`);
+    start();
 });
+
+function displayTable(result) {
+    var table = new Table({
+        head: ['Item ID', 'Product Name', 'Department', 'Price', 'Stock']
+        , colWidths: [10, 45, 40, 8, 8]
+    });
+    for (var i = 0; i < result.length; i++) {
+        table.push([result[i].item_id, result[i].product_name, result[i].department_name, result[i].price, result[i].stock_quantity]);
+    }
+    console.log(table.toString());
+}
+
+function start() {
+    connection.query('SELECT * FROM products', function(error, result) {
+        displayTable(result);
+        var choiceArray = [];
+        for (var i = 0; i < result.length; i++) {
+            choiceArray.push(result[i].product_name);
+        }
+        inquirer.prompt([{
+            name: 'item',
+            type: 'input',
+            message: 'Which is the item id of the product you would like to purchase?'
+        },
+        {
+            name: 'item',
+            type: 'input',
+            message: 'How many you would like to purchase?'
+        }]).then(function(answer) {
+            console.log(answer);
+            var itemID = answer.item_id
+            console.log(itemID);
+            var itemChoice = result[item_id-1];
+            console.log(itemChoice);
+            var newStock = itemChoice.stock_quantity - answer.stock_quantity;
+            if (newStock >= 0) {
+                connection.query('UPDATE products SET ? WHERE item_id = ?', [{stock_quantity: newStock}, item_id]);
+                start();
+            } else {
+                console.log('There are not enough in stock to fulfill your order.');
+                start();
+            }
+        })
+    })
+}    
